6c37155afe0f52a448c7acdda6124ca062233a63
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-11-23 00:20:48 +0300

    Done for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Production.destroy_all
+CrewMember.destroy_all
+
+Production.create(title: 'The Peripheral', genre: 'Opera', budget: 99, image: 'https://flxt.tmsimg.com/assets/p22941336_b_h9_aa.jpg', director: 'Barbs', ongoing: true)
+Production.create(title: 'One of Us Is Lying', genre: 'Thriller', budget: 99, image: 'https://m.media-amazon.com/images/M/MV5BNjVmY2EwMWMtZDJlOS00NGY5LWJkODUtOGQ4NDZlOGE2OTJiXkEyXkFqcGdeQXVyNjEwNTM2Mzc@._V1_FMjpg_UX1000_.jpg', director: 'Dot', ongoing: false)
+Production.create(title: 'Elite', genre: 'Drama', budget: 99, image: 'https://ntvb.tmsimg.com/assets/p15991777_b_h8_ae.jpg?w=1280&h=720', director: 'Dola', ongoing: true)
+
+30.times{CrewMember.create[name:Faker::Name.name , job_title:'Stagehand', salary: 65, production:Production.all.sample]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
